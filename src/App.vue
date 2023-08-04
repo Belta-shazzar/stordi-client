3479689dad3475e99c87dbd4479da65b00b17208
Daniel Oguejiofor <oguejiofor.daniel@chigisoft.com>
2023-08-04 23:34:00 +0100

    init: add auth screen for mobile

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="min-h-screen font-Monts bg-primary">
+    <SiteNavigation />
+    <RouterView />
+  </div>
+</template>
+
+<script setup>
+import { RouterView } from "vue-router";
+import SiteNavigation from "./components/SiteNavigation.vue";
+</script>
+
+<style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<script setup></script>
+
+<template>
+  <div class="container flex flex-col min-h-screen justify-center items-center">
+    <h1 class="text-secondary text-5xl font-bold mb-5">
+      Stord <i class="text-accent fa-solid fa-pen"></i>
+    </h1>
+    <p class="font-medium">We help you make studying seamless</p>
+    <div class="flex gap-3 mt-5">
+      <RouterLink class="hover:underline pb-2" to="/sign-up"
+        >sign up</RouterLink
+      >
+      <RouterLink class="hover:underline pb-2" to="/sign-in"
+        >sign in</RouterLink
+      >
+    </div>
+  </div>
+</template>
